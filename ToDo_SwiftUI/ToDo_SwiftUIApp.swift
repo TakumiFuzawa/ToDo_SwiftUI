9a9a89e867f3f9f260f5e7a0f5a8dfa8fbc465dd
Futaku <futaku0811@gmail.com>
2024-05-09 22:49:48 +0900

    Initial Commit

---
+++ ToDo_SwiftUI/ToDo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_SwiftUIApp.swift
+//  ToDo_SwiftUI
+//
+//  Created by Takumi Fuzawa on 2024/05/09.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
